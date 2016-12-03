e06844bcdd9a00b512fbdb35807489089eb98cb2
Matt Dionis <mattdionis@gmail.com>
2016-12-03 00:33:13 -0500

    Add angular-material and style cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { TweetComponent } from './tweetComponent/tweet.component';
@@ -15,7 +16,8 @@ import { TweetService } from './shared/tweet.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [TweetService],
   bootstrap: [AppComponent]
