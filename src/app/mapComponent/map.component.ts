9a2195dc8da5809a3cfba68867c13186d287fab4
Matt Dionis <mattdionis@gmail.com>
2016-12-09 16:25:24 -0500

    Render dots on map for tweets

--- src/app/mapComponent/map.component.ts
+++ src/app/mapComponent/map.component.ts
@@ -1,21 +1,25 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 import * as D3 from 'd3/index';
 import '../rxjs-operators';
 
 import { MapService } from '../shared/map.service';
+import { Tweet } from '../shared/tweet';
 
 @Component({
   selector: 'map-component',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent implements OnInit {
+export class MapComponent {
+  @Input()
+  twitterState: any;
   errorMessage: string;
   height;
   host;
   htmlElement: HTMLElement;
   mapData;
   margin;
+  points: number[][] = [];
   projection;
   path;
   svg;
@@ -23,12 +27,15 @@ export class MapComponent implements OnInit {
 
   constructor (private _element: ElementRef, private _mapService: MapService) {
     this.host = D3.select(this._element.nativeElement);
-  }
-
-  ngOnInit() {
     this.getMapData();
     this.setup();
     this.buildSVG();
+  }
+
+  ngOnChanges() {
+    if (this.twitterState.tweets) {
+      this.updateDots(this.twitterState.tweets);
+    }
   }
 
   getMapData() {
@@ -63,7 +70,7 @@ export class MapComponent implements OnInit {
     this.mapData = mapData;
     this.projection = D3.geoAlbersUsa()
       .translate([this.width /2 , this.height /2 ])
-      .scale(700);
+      .scale(650);
     this.path = D3.geoPath()
       .projection(this.projection);
 
@@ -74,5 +81,31 @@ export class MapComponent implements OnInit {
         .style('stroke', '#fff')
         .style('stroke-width', '1')
         .style('fill', 'lightgrey');
+  }
+
+  updateDots(tweets) {
+    this.points = [];
+    tweets.forEach((tweet) => {
+      if (tweet.coordinates.length > 0) {
+        const lon = tweet.coordinates[1];
+        const lat = tweet.coordinates[0];
+        const coords = [lon, lat];
+        if (this.projection(coords)) {
+          this.points.push(this.projection(coords));
+        }
+      }
+    });
+    this.svg.selectAll('circle')
+      .data(this.points).enter()
+      .append('circle')
+        .attr('cx', function(d) {
+          return d[0];
+        })
+        .attr('cy', function(d) {
+          return d[1];
+        })
+        .attr('r', '4px')
+        .style('fill', 'blue')
+        .style('opacity', 0.4);
   }
 }
